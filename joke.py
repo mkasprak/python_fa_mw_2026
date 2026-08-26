cc0346c4ecbafaebee934b4ba5e38171d4f83e8b
Meri Kasprak <mkasprak@mchenry.edu>
2026-08-26 10:09:16 -0500

    Add first 3 assignments

---
+++ joke.py
@@ -0,0 +1,8 @@
+#  Assignment information goes at the top
+"""
+Multi line comment - ignored by compiler
+"""
+
+print("What did the fish say when he hit the wall?")
+print("")
+print("Dam!")
---
+++ madlibs.py
@@ -0,0 +1,36 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included (Copy and paste THIS comment from opening to closing quotes).
+[ ] 2. Program asks for at least 5 different inputs (variables).
+[ ] 3. Output uses F-Strings to combine text and variables.
+[ ] 4. Output uses at least one escape sequence (\n or \t).
+[ ] 5. Code contains comments explaining the steps.
+[ ] 6. Program runs without errors.
+-----------------------------------------------------------------------
+"""
+
+# ℹ️ information only
+# 🆘 HELP!
+
+
+# ℹ️ Declare variables
+# name = ""  # ℹ️ intializes the variable (optional)
+# animal = ""
+# color = ""
+
+
+# ℹ️ Get user input and assign to variables
+
+name = input("Please enter a person's name: ")
+animal = input("Please enter a type of animal: ")
+color = input("Please enter a color: ")
+
+
+# ℹ️ Output
+print("Mad Lib for Mary Had a Little Lamb\n\n")
+print(f"{name} had a little {animal}")
+print(f"Whose fleece was {color} as snow")
+print(f"And everywhere that {name} went")
+print(f"The {animal} was sure to go")
